86d57874eb63d591897f71617c10a2ca92681a03
Xiaojia <annacl1822@gmail.com>
2023-03-06 03:00:34 -0700

    Add schema & seeds, connect to index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const options = [
     `View employees by managers`, 
     `View employees by department`, 
     `View combined salaries of all employees in a given department`,
+    `Add a department`,
     `Add a role`, 
     `Add an employee`, 
     `Update an employee role`, 
@@ -19,43 +20,49 @@ const options = [
     `I'm done`,
 ];
 
-const showMenu = () => {inquirer
-    .prompt({
+const showMainMenu = () => {
+    inquirer.prompt([{
         type: 'list',
         name: 'menu', 
         message: 'What would you like to do?',
         choices: options,
-    }).then((choice) => {
+    }]).then((choice) => {
         if (choice === `View all departments`){
-            showMenu();
+            console.log('View all dept')
         } else if (choice === `View all roles`){
-            showMenu();
+            console.log('view all roles')
         } else if (choice === `View all employees`){
-            showMenu();
-        } else if (choice === `View employees by managers`){
-            showMenu();
-        } else if (choice === `View employees by department`){
-            showMenu();
-        } else if (choice === `View combined salaries of all employees in a given department`){
-            showMenu();
-        } else if (choice === `Add a role`){
-            showMenu();
+        // } else if (choice === `View employees by managers`){
+        //     showMainMenu();
+        // } else if (choice === `View employees by department`){
+        //     showMainMenu();
+        // } else if (choice === `View combined salaries of all employees in a given department`){
+        //     showMainMenu();
+        } else if (choice === `Add a department`){
+        }else if (choice === `Add a role`){
         } else if (choice === `Add an employee`){
-            showMenu();
         } else if (choice === `Update an employee role`){
-            showMenu();
-        } else if (choice === `Update employee managers`){
-            showMenu();
-        } else if (choice === `Delete a department`){
-            showMenu();
-        } else if (choice === `Delete a role`){
-            showMenu();
-        } else if (choice === `Delete an employee`){
-            showMenu();
+        // } else if (choice === `Update employee managers`){
+        //     showMainMenu();
+        // } else if (choice === `Delete a department`){
+        //     showMainMenu();
+        // } else if (choice === `Delete a role`){
+        //     showMainMenu();
+        // } else if (choice === `Delete an employee`){
+        //     showMainMenu();
         } else {
             console.log('Program ending')
         }
     })
-}
+};
 
-showMenu()
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'IuvenesDum$umus2',
+        database: 'employee_tracker'
+    }
+)
+
+showMainMenu();
